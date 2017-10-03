d5ff752b083c30dd416a9b189c5efef04957e233
Scott Wilkinson <pswilkinson@gmail.com>
2017-10-03 10:02:54 -0700

    some styling, some db changes

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,17 +6,6 @@ class Item < ActiveRecord::Base
   validates :title, presence: true, length: { maximum: 250 }, allow_blank: false, allow_nil: false
   validates :id, uniqueness: true
 
-  validate do
-    if content.blank? && url.blank?
-      errors.add(:url, 'Submit a URL or Content')
-    end
-    if content.present? && url.present?
-      errors.add(:url, 'Submit a URL or Content but not Both.')
-    end
-  end
-  validates :url, url: {allow_nil: true, allow_blank: true}
-
-
   scope :active, -> { where(disabled: false) }
   scope :disabled, -> { where(disabled: true) }
   scope :newest, -> { order(score: :desc) }
--- db/seeds.rb
+++ db/seeds.rb
@@ -31,14 +31,16 @@ disabled_user = User.create({
   })
 
 [
-  {url: "https://producthunt.com", title: "Product Hunt - Best way to find new products and services"},
-  {url: "https://news.ycombinator.com/", title: "Hacker News - fantastic articles about tech"},
-  {url: "https://reddit.com", title: "Reddit - front page of the internet"},
-  {title: "Reddit - front page of the internet", content: "Here's a content post, which is different than a url post"},
+  {title: "Best smudgable pencil eyeliner I can go pick up at Sephora?", content: "I wanna know!"},
+  {title: "Has anyone tried Kristen Ess's hair products from Target? Thoughts?", content: "I wanna know!"},
+  {title: "I would like to get my teenage niece her first makeup items. Can anyone recommend makeup for a teenager with dark hair, brown eyes and olive skin tone?", content: "I wanna know!"},
+  {title: "Does rogaine work on eyebrows?", content: "I wanna know!"},
+  {title: "Has anyone tried Kristen Ess's hair products from Target? Thoughts?", content: "I wanna know!"},
+  {title: "Best smudgable pencil eyeliner I can go pick up at Sephora?", content: "I wanna know!"},
 ].each do |item|
-  admin.items.create(item)
+  #admin.items.create(item)
   user.items.create(item)
-  disabled_user.items.create(item)
+  #disabled_user.items.create(item)
 end
 
 [admin, user, disabled_user].each do |u|
